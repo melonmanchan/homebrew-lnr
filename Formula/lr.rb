24436adbd55db9fa74d892419ddeed4969da0483
Matti Jokitulppo <matti.jokitulppo@metacoregames.com>
2025-03-11 16:03:52 +0200

    Release 0.0.4

--- Formula/lr.rb
+++ Formula/lr.rb
@@ -2,15 +2,15 @@ class Lr < Formula
   desc "Linear CLI tool"
   homepage "https://github.com/melonmanchan/lr"
   head "https://github.com/melonmanchan/lr.git", branch: "main"
-  version "0.0.3"
+  version "0.0.4"
   license "MIT"
   
   if Hardware::CPU.arm?
-    url "https://github.com/melonmanchan/lr/releases/download/v0.0.3/lr-macos-arm64.zip"
-    sha256 "8e6defca1832156dc210281ec1ad252fbf61fd5d0d77c8cbc8819535026657f9"
+    url "https://github.com/melonmanchan/lr/releases/download/v0.0.4/lr-macos-arm64.zip"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   else
-    url "https://github.com/melonmanchan/lr/releases/download/v0.0.3/lr-macos-x64.zip"
-    sha256 "bdc4e2be977f227f9a1d70f5cfad9ea48131e6d5ba1a26fd2e5e114ce159e86f"
+    url "https://github.com/melonmanchan/lr/releases/download/v0.0.4/lr-macos-x64.zip"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   end
 
   def install
